389dc0931a6c8db44cfa7b44b1576071a3b91dbe
Matthew Fitch <matthew.fitch26@d155.org>
2024-09-24 07:27:28 -0700

    Initial Commit

---
+++ clicker-game-fitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clicker-game-fitch
+//
+//  Created by MATTHEW FITCH on 9/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
